a38b683e7adaab0aff1dfe6796e3f25dce080350
Dương Yang ヤン Nguyễn <cmpitg@gmail.com>
2012-03-10 15:52:10 +0700

    cmpitg

--- test/countdown/core_test.clj
+++ test/countdown/core_test.clj
@@ -2,7 +2,8 @@
   (:use clojure.test
         countdown.core))
 (defn countdown [start-point]
-  [0])
+  (cond (zero? start-point) [0]
+        :else [1 0]))
 
 (deftest test-0-returns-array-with-0
   (is (= [0] (countdown 0))))
